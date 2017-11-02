2b80231d6cff9fd14a1c813d31f15ac991bd60b7
MelGitacc <Emgeexy@yahoo.com>
2017-11-02 12:47:00 +0000

    first Commit

--- app/src/main/java/com/example/mel/twoactivites/MainActivity.java
+++ app/src/main/java/com/example/mel/twoactivites/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.mel.twoactivites;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int someScore = 0;//variable to be used for the value od someScore
+    private int someScore = 0;//variable to be used for the value of someScore
+
+    private static String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG,"onCreate");//prints out the message when the app is running
 
         // test for this null state
         if ( savedInstanceState != null ) {
@@ -23,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
             someScore = savedInstanceState.getInt( "HIGH_SCORE", 0 );
 
         }
+
     }
     // method for the second activity
     public void displaySecondActivity( View view ) {
@@ -34,12 +43,57 @@ public class MainActivity extends AppCompatActivity {
         //"%s" will convert the someScore integer to string
         myTextView.setText(String.format("%s", someScore));
         
-       // Intent myIntent = new Intent( this, SecondActivity.class);
+       Intent myIntent = new Intent( this, SecondActivity.class);
 
         //this code will send data to an activity
-       // myIntent.putExtra( "HELLO_MESSAGE", " First activity say Hello!");
+        myIntent.putExtra( "HELLO_MESSAGE", " First activity say Hello!");
 
-      //  startActivity( myIntent );
+        startActivity( myIntent );
+
+        //lets try adding toast message when the second activity button is clicked
+        //first initialise a Toast object to a toast class
+        Toast toast = Toast.makeText(MainActivity.this,"You will be directed to second activity",Toast.LENGTH_LONG );
+        toast.setGravity(Gravity.CENTER, 0,0);//This will set the positioning of your toast message
+        toast.show();//this will show the toast message on the screen
+    }
+
+    //overriding lifecycle methods
+    //this log cat messages shows the state of the activity that you are running
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"onRestart");
     }
 
     //Before our Activity is destroyed when the screen is rotated, Android allows you to override a method called onSaveInstanceState
--- app/src/main/java/com/example/mel/twoactivites/SecondActivity.java
+++ app/src/main/java/com/example/mel/twoactivites/SecondActivity.java
@@ -3,15 +3,20 @@ package com.example.mel.twoactivites;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private static String TAG = "SecondActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_second);
+
+        Log.d(TAG, "onCreate");//prints out the message when the app is running
+
         //using the inbuilt method of Intents We'll retrieve the value for our key here
         Intent secondIntent = getIntent();
 
@@ -23,5 +28,43 @@ public class SecondActivity extends AppCompatActivity {
         //this is setting that message variable we created
         // which is stored from the value of our HELLO_MESSAGE key.
         myTextView.setText( message );
+    }
+
+    //overriding lifecycle methods
+    //this log cat messages shows the state of the activity that you are running
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"onRestart");
     }
 }
